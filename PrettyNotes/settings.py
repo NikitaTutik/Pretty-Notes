ab406e4e6b73a093f8ef88ad795d654af495eae3
NikitaTutik <famounst@gmail.com>
2022-05-16 15:42:15 +0300

    Auth + refactor #3

--- PrettyNotes/settings.py
+++ PrettyNotes/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2+5)ii=bt2ay_a29pb=u)=rp1a@j3zincd9ez#^$0%vw*p9-fr"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
